36810942cefba150f30102799d0c329d22398f34
cwu155 <cwu17@stuy.edu>
2018-11-11 22:36:13 -0500

    Updated constructor

--- WordSearch.java
+++ WordSearch.java
@@ -1,29 +1,18 @@
-import java.util.*; //random, scanner, arraylist
-import java.io.*; //file, filenotfoundexception
+import java.util.*;
+import java.io.*;
 public class WordSearch{
     private char[][]data;
-     //the random seed used to produce this WordSearch
-     private int seed;
-
-     //a random Object to unify your random calls
-     private Random randgen;
-
-     //all words from a text file get added to wordsToAdd, indicating that they have not yet been added
-     private ArrayList<String>wordsToAdd;
-
-     //all words that were successfully added get moved into wordsAdded.
-     private ArrayList<String>wordsAdded;
+    private int seed;
+    private Random randgen;
+    private ArrayList<String>wordsToAdd;
+    private ArrayList<String>wordsAdded;
 
 
-
-    public WordSearch(int rows,int cols){
-      if ((rows < 0) || (cols < 0)){
-        throw new IllegalArgumentException("No negative indices!");
-      }
-
+    public WordSearch(int rows,int cols, String fileName){
+    // Choose a randSeed using the clock Random
+      randgen = new Random(seed);
       data = new char[rows][cols];
       clear();
-
     }
 
     /**Set all values in the WordSearch to underscores'_'*/
